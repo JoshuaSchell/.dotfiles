00db168ade851fbf0029960164857df162ce91fd
Josh Schell <josh@schell.me>
2024-12-21 16:31:47 -0700

    lspconfig gopls for neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,7 @@ return {
     },
     config = function()
       require("lspconfig").clangd.setup {}
+      require("lspconfig").gopls.setup {}
       require("lspconfig").lua_ls.setup {}
       require("lspconfig").pyright.setup {}
 
