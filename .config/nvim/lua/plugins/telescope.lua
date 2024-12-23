fa0e02d7d2a74de261af6eba812d97b6ea12632e
Josh Schell <josh@schell.me>
2024-12-22 18:33:36 -0700

    added help tags keymap for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
   },
   config = function()
     vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+    vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
     vim.keymap.set("n", "<space>en", function()
       require('telescope.builtin').find_files {
         cwd = "~/.dotfiles/.config/nvim/"
