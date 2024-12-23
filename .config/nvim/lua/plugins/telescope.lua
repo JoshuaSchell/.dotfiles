e96d4fb774d365d4d05d8aac0907224753006300
Josh Schell <josh@schell.me>
2024-12-22 18:14:23 -0700

    added keymaps for file searching with telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,11 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
+    vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+    vim.keymap.set("n", "<space>en", function()
+      require('telescope.builtin').find_files {
+        cwd = "~/.dotfiles/.config/nvim/"
+      }
+    end)
   end,
 }
