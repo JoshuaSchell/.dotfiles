431af638aa8ea992f4f97f9835323ca1cf917a85
Josh Schell <josh@schell.me>
2024-12-31 18:18:10 -0700

    added rust to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "c", "cpp", "lua", "go", "python", "zig", "nix", "cuda", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        ensure_installed = { "c", "cpp", "lua", "go", "python", "zig", "rust", "nix", "cuda", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
         auto_install = false,
         highlight = {
           enable = true,
