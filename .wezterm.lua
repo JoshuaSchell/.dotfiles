61122cfd547fdbec6cdc76ab84f685daea207a0d
Josh Schell <josh@schell.me>
2025-01-24 16:08:33 -0700

    switched wezterm to gruvbox

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,13 +3,13 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-    automatically_reload_config = true,
-    enable_tab_bar = false,
-    window_close_confirmation = "NeverPrompt",
-    window_decorations = "RESIZE",
-    default_cursor_style = "SteadyBar",
-    color_scheme = 'OneHalfDark',
-    font_size = 16,
+  automatically_reload_config = true,
+  enable_tab_bar = false,
+  window_close_confirmation = "NeverPrompt",
+  window_decorations = "RESIZE",
+  default_cursor_style = "SteadyBar",
+  color_scheme = 'Gruvbox dark, medium (base16)',
+  font_size = 16,
 }
 
 return config
