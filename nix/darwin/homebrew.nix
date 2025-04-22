5aadfe20e5e5a612d4a67400ef2a7b1e0763f7ab
Josh Schell <josh@schell.me>
2025-04-22 10:19:56 -0600

    raspberry pi

--- nix/darwin/homebrew.nix
+++ nix/darwin/homebrew.nix
@@ -5,22 +5,20 @@
     brews = [ ];
     taps = [ ];
     casks = [
-      "anaconda"
       "brave-browser"
       "cursor"
       "discord"
       "dotnet-sdk"
       "docker"
-      "figma"
       "font-jetbrains-mono"
       "ghostty"
       "obsidian"
+      "raspberry-pi-imager"
       "raycast"
       "rider"
       "vesktop"
       "visual-studio-code"
       "wezterm"
-      "wireshark"
       "zed"
       "zen-browser"
       "zoom"
